3cf8e1d85937401a298c16cacb239168f48c94ac
fernandolilo <FERNANDO.SISTENPRO@OUTLOOK.COM>
2022-02-14 19:27:05 -0300

    Entities product and category

--- productInStock/src/main/java/com/ayatempro/productInStock/ProductInStockApplication.java
+++ productInStock/src/main/java/com/ayatempro/productInStock/ProductInStockApplication.java
@@ -1,13 +1,20 @@
 package com.ayatempro.productInStock;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProductInStockApplication {
+public class ProductInStockApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductInStockApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
